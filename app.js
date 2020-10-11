2dce324e38c945cee0a6c5eda6d8501cda02de4d
“Lauryn <laurynmills111@gmail.com>
2020-10-11 19:55:04 -0400

    incorporate node and express

---
+++ app.js
@@ -0,0 +1,30 @@
+//IMPORTS
+let express = require('express');
+let app = express();
+
+
+//STATIC FILES
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
+
+//SET
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+//ROUTES
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/joinus', function(req, res){
+    res.render('joinus');
+});
+
+//LISTEN ON PORT 3000
+app.listen(3000, function(){
+    console.log('Server is running');
+});
